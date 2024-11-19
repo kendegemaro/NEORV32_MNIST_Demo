f77071584e83757c35d0b1697fb56b431414657b
Kende Gömöri <kende.goemoeri@boschrexroth.de>
2024-11-19 18:25:05 +0100

    Add source files

---
+++ target_platforms/NEORV32/MNIST_NeuralNet.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void MNIST_NeuralNet(k2c_tensor* flatten_input_input, k2c_tensor* dense_1_output); 
+void MNIST_NeuralNet_initialize(); 
+void MNIST_NeuralNet_terminate(); 
